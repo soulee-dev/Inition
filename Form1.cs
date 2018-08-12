fba0efff6420af9053a5c16f6c7d1659ab437cdd
DESKTOP-K950T8G\a0103 <a0103932@naver.com>
2018-08-13 00:05:58 +0900

    First commit

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Inition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Inition init = new Inition();
+            button1.Size = new Size(init.ScreenScale(50), init.ScreenScale(30));
+            button1.Location = new Point(init.ScreenScale(20), init.ScreenScale(20));
+            button1.Font = new Font(button1.Font.FontFamily, init.ScreenScale(5));
+            Size = new Size(init.ScreenScale(250), init.ScreenScale(200));
+
+            SystemEvents.DisplaySettingsChanged += new EventHandler(SystemEvents_DisplaySettingsChanged);
+        }
+
+
+        private void SystemEvents_DisplaySettingsChanged(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Inition init = new Inition();
+            button1.Size = new Size(init.ScreenScale(50), init.ScreenScale(30));
+            button1.Location = new Point(init.ScreenScale(20), init.ScreenScale(20));
+            button1.Font = new Font(button1.Font.FontFamily, init.ScreenScale(5));
+            Size = new Size(init.ScreenScale(250), init.ScreenScale(200));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Inition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inition
+{
+    class Inition
+    {
+        public int ScrH()
+        {
+            return Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        public int ScrW()
+        {
+            return Screen.PrimaryScreen.Bounds.Width;
+        }
+
+        public int ScreenScale(int size)
+        {
+            return size * (ScrW() / 640);
+        }
+    }
+}
